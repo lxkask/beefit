3ec37550e911796d0da7b1fc039df9ad7e399583
Lukáš Kubík <lukoluko8@gmail.com>
2025-07-30 14:09:33 +0200

    Add Svelte static adapter and update dependencies; refactor cenik route for improved data handling

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,29 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { preprocessMeltUI, sequence } from '@melt-ui/pp'; 
+import adapter from '@sveltejs/adapter-static';
+import sveltePreprocess from 'svelte-preprocess'; // ← zde
+
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: sequence([
-		vitePreprocess(),
-		preprocessMeltUI()
-    ]),
+  preprocess: sequence([
+    sveltePreprocess(),
+    preprocessMeltUI()
+  ]),
 
-    kit: {
-		adapter: adapter(),
-		typescript: {
-            config: (config) => {
-                config.include.push('../src/lib/types/**/*.ts');
-                return config;
-            }
-        }
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+
+    typescript: {
+      config: (config) => {
+        config.include.push('../src/lib/types/**/*.ts');
+        return config;
+      }
+    }
+  }
 };
 
 export default config;
-
